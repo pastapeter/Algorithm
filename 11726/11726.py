6035400b086d1a43e4f4f4662d9b7d3117ff409c
DOHYUN <pastapeter@naver.com>
2021-07-06 17:11:40 +0900

    11726 swift & python3

---
+++ 11726/11726.py
@@ -0,0 +1,14 @@
+def solution():
+    num = int(input())
+    arr = [0 for x in range(num+1)]
+    if num == 1:
+        return 1
+    else:
+        arr[1] = 1
+        arr[2] = 2
+        if num >= 3:
+            for i in range(3, num+1):
+                arr[i] = arr[i-1] + arr[i-2]
+    return arr[num] % 10007
+
+print(solution())
---
+++ 11726/11726/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  11726
+//
+//  Created by abc on 2021/07/06.
+//
+
+import Foundation
+
+func teliing()->Int{
+    let input = Int(readLine()!)!
+    var arr = [Int](repeating: 0, count: input+1)
+    if input == 1{
+        return 1
+    } else {
+        arr[1] = 1
+        arr[2] = 2
+        if input >= 3 {
+            for i in 3...input{
+                arr[i] = arr[i-1] + arr[i-2]
+            }
+        }
+    }
+    return arr[input] % 10007
+}
+
+print(teliing())
