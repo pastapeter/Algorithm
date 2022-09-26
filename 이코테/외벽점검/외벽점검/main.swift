a9731ac403379276045111d2dd06f3bb2c68d0d8
Dohyun Chung <pastapeter@naver.com>
2022-09-26 18:07:05 +0900

    외벽점검 설명

--- 이코테/외벽점검/외벽점검/main.swift
+++ 이코테/외벽점검/외벽점검/main.swift
@@ -10,14 +10,18 @@ import Foundation
 func solution(_ n:Int, _ weak:[Int], _ dist:[Int]) -> Int {
 
   var weaks: Set<[Int]> = [weak]
+    
   for (index, d) in dist.reversed().enumerated() {
     var newWeaks: Set<[Int]> = []
     for w in weaks {
       for start in w {
-        let end = (n + (start-d)) % n
-        let filterWeaks = start > end ? w.filter {$0 < end || start+1...n ~= $0} : w.filter {start+1..<end ~= $0 }
-        if filterWeaks.isEmpty {return index + 1}
-        newWeaks.insert(filterWeaks)
+        let end = (n + (start-d)) % n // 시계 반대방향으로
+        let filterWeaks = start > end ?
+          w.filter {$0 < end || start+1...n ~= $0} : 
+          w.filter {start+1..<end ~= $0 // 시계 반대방향으로 돌았을때의 미처 돌지 못한 취약점들
+          }
+        if filterWeaks.isEmpty {return index + 1} // 미처확인하지 못한 취약점이 남아있다면??
+        newWeaks.insert(filterWeaks) // 그때 취약점을 새로운 취약점에 넣어라
       }
     }
     weaks = newWeaks
