bc21925ab65310b1f71d4acdbe45b4110605d42b
Dohyun Chung <pastapeter@naver.com>
2023-04-02 18:17:15 +0900

    ps1766

--- 알고리즘/ps1766/ps1766/main.swift
+++ 알고리즘/ps1766/ps1766/main.swift
@@ -23,7 +23,10 @@ import Foundation
  하나 뺴고 위상정렬 실시 [4]
  다시 큐를 정렬 [4]
  하나 뺴고 위상정렬 실시 [2]
- ... 
+ ...
+ 
+ 2번 풀이
+ 위상정렬에서 queue에 넣을 때 그 queue가 우선순위 큐면 개꿀일듯
  */
 
 let nm = readLine()!.split(separator: " ").map { Int(String($0))!}
@@ -44,23 +47,100 @@ for _ in 0..<m {
   indegree[b] += 1
 }
 
+struct Node: Comparable {
+  static func < (lhs: Node, rhs: Node) -> Bool {
+    lhs.priority < rhs.priority
+  }
+  var node: Int = 0
+  var priority: Int = 0
+}
+
+public struct PriorityQueue<T: Comparable> {
+  fileprivate var heap = [T]()
+  private let ordered: (T, T) -> Bool
+  public init(ascending: Bool = false, startingValues: [T] = []) {
+    ordered = ascending == true ? { $0 > $1 } : { $0 < $1 }
+    
+    heap = startingValues
+    var i = heap.count/2 - 1
+    while i >= 0 {
+      sink(i)
+      i -= 1
+    }
+  }
+  
+  public var count: Int { return heap.count }
+  
+  public var isEmpty: Bool {return heap.isEmpty}
+  
+  
+  /// 우선순위에 큐 추가
+  /// - Parameter element: 해당 요소는 우선순위 큐에 삽입되어야함
+  public mutating func push(_ element: T) {
+    heap.append(element)
+    swim(heap.count - 1)
+  }
+  
+  public mutating func pop() -> T? {
+    if heap.isEmpty {return nil}
+    if heap.count == 1 {
+      return heap.removeFirst()
+    }
+    heap.swapAt(0, heap.count - 1)
+    let temp = heap.removeLast()
+    sink(0)
+    return temp
+  }
+  
+  public mutating func remove(_ item: T) {
+    if let index = heap.firstIndex(of: item) {
+      heap.swapAt(index, heap.count - 1)
+      heap.removeLast()
+      swim(index)
+      sink(index)
+    }
+  }
+  
+  private mutating func sink(_ index: Int) {
+    var index = index
+    while 2*index + 1 < heap.count {
+      var j = 2 * index + 1
+      if j < heap.count - 1 && ordered(heap[j], heap[j+1]) {
+        j += 1
+      }
+      if !ordered(heap[index], heap[j]) {break}
+      heap.swapAt(index, j)
+      index = j
+    }
+  }
+  
+  private mutating func swim(_ index: Int) {
+    var index = index
+    while index > 0 && ordered(heap[(index - 1) / 2], heap[index]) {
+      heap.swapAt((index - 1) / 2, index)
+      index = (index - 1) / 2
+    }
+  }
+}
+
+
 var ans = ""
-var queue: [Int] = []
+var queue = PriorityQueue<Int>(ascending: true)
+
 for i in 1...n {
   if indegree[i] == 0 {
-    queue.append(i)
+    queue.push(i)
   }
 }
 
 while !queue.isEmpty {
-  queue.sort(by: <)
-  let poped = queue.removeFirst()
+  let poped = queue.pop()!
   ans += "\(poped) "
   
   for i in graph[poped]! {
     indegree[i] -= 1
     if indegree[i] == 0 {
-      queue.append(i)
+      queue.push(i)
     }
   }
   
