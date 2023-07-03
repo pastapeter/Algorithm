02e3af099ad443b56bd818c0aa9a821d2d24f075
Dohyun Chung <pastapeter@naver.com>
2023-07-03 22:51:58 +0900

    다익스트라연숩

--- 알고리즘/13549.py
+++ 알고리즘/13549.py
@@ -1,12 +1,12 @@
 import sys
 from queue import PriorityQueue
+import heapq
 
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-dist = [1e9 for i in range(100001)]
-dist[n] = 0
+
 
 if k <= n:
     print(n - k)
@@ -19,6 +19,35 @@ else:
             graph[i].append([1, i + 1])
         if 2 * i <= k + 1:
             graph[i].append([0, 2 * i])
+
+    graph2 = [[] for _ in range(200001)]
+    for i in range(0, 100001):
+        graph2[i].append((i + 1, 1))
+        graph2[i].append((i - 1, 1))
+        graph2[i].append((2 * i, 0))
+    dist = [1e9] * 200001
+    dist[n] = 0
+    q = []
+    heapq.heappush(q, (0, n))
+    # heapq에 넣어줘
+    # heapq는 앞에꺼를 기반으로 minheap임
+    # 그래서 0, n으로 추가
+    # q가 없을때까지
+    while q:
+        # pop을 했을때
+        d, now = heapq.heappop(q)
+        # 만약 d가 현재 dist[now] 보다 크다면, 갱신 필요없음
+        if d > dist[now]:
+            continue
+        # 현재 위치까지 가는 최단거리가 줄어들었다면
+        # 현재 위치에서 다른 곳으로 가는 곳도 갱신해줘야함
+        for i in graph2[now]:
+            # 현재 위치까지 가는 최단거리 + 다음위치까지가는 거리
+            cost = dist + i[1]
+            # 현재 위치까지 가는 최단거리 + 다음위치까지가는 거리 < 다음위치까지 가는 최단거리(지금까지나온것중)
+            if cost < dist[i[0]]:
+                dist[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
     def dijkstra(start, end):
         d = [1e9] * (end + 2)
